03fcf831494dd612f03e5fe43e839ac0069b33b9
jiangguangyangxiaona <308886363@qq.com>
2020-07-11 16:03:59 +0800

    444

--- src/main/java/com/jgy/huashan/Dugujiujian.java
+++ src/main/java/com/jgy/huashan/Dugujiujian.java
@@ -3,6 +3,7 @@ package com.jgy.huashan;
 public class Dugujiujian {
     public static void main(String[] args) {
         System.out.println(123);
+        System.out.println(123);
         System.out.println(000);
         System.out.println(000);
     }
