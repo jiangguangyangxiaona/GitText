b55a2c3f90c6931fe0da275c6222d749b9418652
jiangguangyangxiaona <308886363@qq.com>
2020-07-13 09:35:21 +0800

    yijining

--- src/main/java/com/jgy/huashan/riyue/XiXing.java
+++ src/main/java/com/jgy/huashan/riyue/XiXing.java
@@ -3,5 +3,6 @@ package com.jgy.huashan.riyue;
 public class XiXing {
     public static void main(String[] args) {
         System.out.println(654);
+        System.out.println("yijinjing");
     }
 }
