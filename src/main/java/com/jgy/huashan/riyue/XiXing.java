bd3208907c6700ca9ceb6d1b00b08ff64ae3acd2
jiangguangyangxiaona <308886363@qq.com>
2020-07-13 10:00:35 +0800

    xixingdafa

--- src/main/java/com/jgy/huashan/riyue/XiXing.java
+++ src/main/java/com/jgy/huashan/riyue/XiXing.java
@@ -4,5 +4,6 @@ public class XiXing {
     public static void main(String[] args) {
         System.out.println(654);
         System.out.println("yijinjing");
+        System.out.println("吸星大法！！！");
     }
 }
